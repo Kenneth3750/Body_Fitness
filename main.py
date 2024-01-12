d45e773caf7d69463148d23ba3c97e1cb88aaf14
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-01-12 13:31:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/index.html')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+
+app.run(debug=True, port=10000)
