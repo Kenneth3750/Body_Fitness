517c87b6b24a9472fccca1727627722ff17f6d4a
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-01-15 12:18:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 
 from flask import Flask, render_template, request
-import mysql.connector
-from mysql.connector import Error
+import pymysql
+from pymysql import Error
+from decouple import config
+
+
 
 app = Flask(__name__)
 
 def database_connection():
     try:
-        connection = mysql.connector.connect(host='localhost',
-                                    user='root',
-                                    password='ken_1002161089',
-                                    db='body_fitness')
+        connection = pymysql.connect(host=config('host'),
+                                    user=config('user'),
+                                    password=config('password'),
+                                    db=config('db') )
         return connection
     except Error as e:
         print("Database unreachable, " + str(e))
@@ -28,22 +31,33 @@ def index():
 def registro():
     return render_template('registro.html')
 
+
+#user register :)
 @app.route('/registro.html', methods=['POST'])
 def form_data():
     if request.method == 'POST':
         user = request.form.to_dict()
         user_no_empty = {k: v for k, v in user.items() if v}
         user = user_no_empty
-
+    try:
         connection = database_connection()
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO users (nombre, apellido, edad, cedula, correo, celular, direccion) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (user['nombre'], user['apellido'], user['edad'], user['cedula'], user['correo'], user['celular'], user['direccion']))
+        if connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO users (nombre, apellido, edad, cedula, correo, celular, direccion) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                values = (user['nombre'], user['apellido'], user['edad'], user['cedula'], user['correo'], user['telefono'], user['direccion'])
+                cursor.execute(sql,values )
 
-        cursor.close()
-        connection.close()
+            connection.commit()
+            connection.close()
+        else:
+            print("Database connection failed")
+    except Error as e:
+        return 'Error ' + str(e)
 
-        return render_template('registro.html')
+    return render_template('registro.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=10000)
