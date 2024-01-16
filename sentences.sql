50b2b68e84bf2b99d5aa0cf8c8da6dbff0de7575
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-01-16 13:12:44 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -16,3 +16,18 @@ create table if not exists users(
     direccion varchar(100),
     created_at datetime default current_timestamp
 );
+
+create table if not exists plans(
+    id int not null primary key auto_increment,
+    plan_name varchar(50) not null
+);
+
+
+insert into plans(plan_name) values('1 Mes');
+insert into plans(plan_name) values('2 Meses');
+insert into plans(plan_name) values('3 Meses');
+insert into plans(plan_name) values('6 Meses');
+insert into plans(plan_name) values('10 dias');
+insert into plans(plan_name) values('12 dias');
+insert into plans(plan_name) values('15 dias');
+insert into plans(plan_name) values('otro');
