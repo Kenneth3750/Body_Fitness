31da68c0fafa9223a456eae77d045b404e4699dc
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-01-17 22:33:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,37 @@ def get_plan_duration(id_plan):
     except Error as e:
         print( 'Error ' + str(e))
         return None
+def plan_info(plan, user):
+    month_plans = [1, 2, 3, 4]
+    day_plans = [5, 6, 7]
+    if plan in month_plans:
+        plan_duration= get_plan_duration(plan)
+        plan_duration = plan_duration[0][0]
+        plan_duration = int(plan_duration)
+
+        end_date = datetime.now() + timedelta(days=30*plan_duration) 
+        end_date = end_date.strftime("%Y-%m-%d")
+        frequency = None
+    elif plan in day_plans:
+        plan_duration= get_plan_duration(plan)
+        plan_duration = plan_duration[0][0]
+        plan_duration = int(plan_duration)
+        end_date = datetime.now() + timedelta(days=plan_duration)
+        end_date = end_date.strftime("%Y-%m-%d")
+        if plan == 5:
+            frequency = 10
+        elif plan == 6:
+            frequency = 12
+        else:
+            frequency = 15
+    else:
+        plan_duration = user['duracion']
+        plan_duration = int(plan_duration)
+        end_date = datetime.now() + timedelta(days = 30*plan_duration)
+        end_date = end_date.strftime("%Y-%m-%d")
+        frequency = None
+    return end_date, frequency
+
 
 
 @app.route('/')
@@ -84,35 +115,8 @@ def form_data():
         user = user_no_empty
         plan = user['plan']
         plan = int(plan)
-
-        month_plans = [1, 2, 3, 4]
-        day_plans = [5, 6, 7]
-        if plan in month_plans:
-            plan_duration= get_plan_duration(plan)
-            plan_duration = plan_duration[0][0]
-            plan_duration = int(plan_duration)
-
-            end_date = datetime.now() + timedelta(days=30*plan_duration) 
-            end_date = end_date.strftime("%Y-%m-%d")
-            frequency = None
-        elif plan in day_plans:
-            plan_duration= get_plan_duration(plan)
-            plan_duration = plan_duration[0][0]
-            plan_duration = int(plan_duration)
-            end_date = datetime.now() + timedelta(days=plan_duration)
-            end_date = end_date.strftime("%Y-%m-%d")
-            if plan == 5:
-                frequency = 10
-            elif plan == 6:
-                frequency = 12
-            else:
-                frequency = 15
-        else:
-            plan_duration = user['duracion']
-            plan_duration = int(plan_duration)
-            end_date = datetime.now() + timedelta(days = 30*plan_duration)
-            end_date = end_date.strftime("%Y-%m-%d")
-            frequency = None
+        end_date, frequency = plan_info(plan, user)
+        
         current_date = datetime.now()
         current_date = current_date.strftime("%Y-%m-%d")
         
@@ -144,26 +148,57 @@ def form_data():
 def usuario():
     return render_template('usuarios.html')
 
+#view and renew an user
 @app.route('/usuarios.html', methods=['POST', 'GET'])
 def search_user():
     if request.method == 'POST':
-        user_dni = request.form.to_dict()
-        user_dni = list(user_dni.keys())[0]
-        try:
-            connection = database_connection()
-            if connection:
-                with connection.cursor() as cursor:
-                    sql = "SELECT * FROM users INNER JOIN user_plans on users.id = user_plans.user_id WHERE cedula = (%s)"
-                    cursor.execute(sql, user_dni)
-                    result = cursor.fetchall()
-                    connection.close()
-                    return jsonify(result), 200
-            else:
-                print("Database connection failed")
-                return jsonify({'message': 'Database connection failed'}), 500
-        except Error as e:
-            print( 'Error ' + str(e))
-            return jsonify({'message': 'Error' + str(e)}), 500
+        data = request.form.to_dict()
+        #form1 --> view user
+        if data['form_id'] == 'form1':
+            user_dni = data['id']
+            try:
+                connection = database_connection()
+                if connection:
+                    with connection.cursor() as cursor:
+                        sql = "SELECT * FROM users INNER JOIN user_plans on users.id = user_plans.user_id WHERE cedula = (%s)"
+                        cursor.execute(sql, user_dni)
+                        result = cursor.fetchall()
+                        connection.close()
+                        return jsonify(result), 200
+                else:
+                    print("Database connection failed")
+                    return jsonify({'message': 'Database connection failed'}), 500
+            except Error as e:
+                print( 'Error ' + str(e))
+                return jsonify({'message': 'Error' + str(e)}), 500
+        #form2 --> renew user
+        elif data['form_id'] == 'form2':
+            print(data)
+            plan = data['plan']
+            int_plan = int(plan)
+            user_dni = data['id']
+            end_date, frequency = plan_info(int_plan, data)
+            try:
+                connection = database_connection()
+                if connection:
+                    with connection.cursor() as cursor:
+                        sql = "INSERT INTO user_plans (user_id, plan_id, start_plan_date, end_plan_date, frequency, payment_day) VALUES (%s, %s, %s, %s, %s, %s)"
+                        values = (user_dni, plan, datetime.now(), end_date, frequency, None)
+                        cursor.execute(sql, values)
+                        connection.commit()
+                        connection.close()
+                        return jsonify({'message': 'User updated successfully'}), 200
+                else:
+                    print("Database connection failed")
+                    return jsonify({'message': 'Database connection failed'}), 500
+            except Error as e:
+                print( 'Error ' + str(e))
+                return jsonify({'message': 'Error' + str(e)}), 500
+        
+
+
+        
+
 
 
 
