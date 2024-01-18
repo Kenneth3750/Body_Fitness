dfd791f6634192d1b41ef8974efb08880aef6556
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-01-18 12:29:45 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -47,3 +47,4 @@ insert into plans(plan_name, duration) values('10 dias', 15);
 insert into plans(plan_name, duration) values('12 dias', 20);
 insert into plans(plan_name, duration) values('15 dias', 30);
 insert into plans(plan_name, duration) values('otro', null);
+
