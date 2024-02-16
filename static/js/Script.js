e496a98bd560a5bf1783974e9220707de304cd74
Samuel Solano <sdsolano@uninorte.edu.co>
2024-02-16 11:33:39 -0500

    .

--- static/js/Script.js
+++ static/js/Script.js
@@ -276,10 +276,17 @@ function table_data_Users(data) {
                 <td>${plan_state}</td>
                 <td>${dias_rest}</td>
                 <td>${ultimoIngreso.getFullYear()}-${mes(ultimoIngreso.getMonth())}-${dias2(ultimoIngreso.getDate())}</td>
-                <td>${data[i][16]} <button class="btn" onclick="confirmPayment()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"style="fill: #66a324;"/>
-              </svg></button> </td>
-            </tr>
+                <td>
+                    <div style="display: flex; align-items: center;">
+                        <span>${data[i][16]}</span>
+                        <button class="btn" onclick="confirmPayment()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" style="fill: #66a324;"/>
+                            </svg>
+                        </button>
+                    </div>
+                </td>
+                </tr>
 
         `; 
         
