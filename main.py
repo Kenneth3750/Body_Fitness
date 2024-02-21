4dc2cb351d15911549e368b7fcd2b82752d3ed10
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2024-02-20 19:41:19 -0500

    .

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME']="bodyfitness12478@gmail.com"
-app.config['MAIL_PASSWORD']="ctpqvggtrfwnkjgw"
+app.config['MAIL_PASSWORD']="gmail password"
 mail.init_app(app)
 
 
